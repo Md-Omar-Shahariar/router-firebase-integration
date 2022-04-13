c79de6db1da201af2223488a3237868186c99c61
Md-Omar-Shahariar <osafridi00@gmail.com>
2022-04-13 19:21:19 +0600

    auth state change

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,9 +1,13 @@
 import React from "react";
+import useFirebase from "../../hooks/useFirebase";
 
 const Login = () => {
+  const { user, signInWithGoogle } = useFirebase();
+
   return (
     <div>
       <h3>Please Log in</h3>
+      <img src={user?.photoURL} alt="" />
       <form action="">
         <input type="email" placeholder="Your Email" name="" id="" />
         <br />
@@ -12,7 +16,7 @@ const Login = () => {
         <input type="submit" value="login" />
       </form>
       <div style={{ margin: "30px" }}>
-        <button>Google Sign In</button>
+        <button onClick={signInWithGoogle}>Google Sign In</button>
       </div>
     </div>
   );
--- src/hooks/useFirebase.js
+++ src/hooks/useFirebase.js
@@ -1,13 +1,37 @@
 import { useEffect, useState } from "react";
+import {
+  getAuth,
+  GoogleAuthProvider,
+  onAuthStateChanged,
+  signInWithPopup,
+  signOut,
+} from "firebase/auth";
+import app from "../firebase.init";
+
+const auth = getAuth(app);
+const googlePorvider = new GoogleAuthProvider();
 
 const useFirebase = () => {
-  const [user, setUser] = useState();
+  const [user, setUser] = useState({});
 
-  useEffect(() => {}, []);
-  const signInWithGoogle = () => {
-    console.log("Sign in with Google");
+  const handleSignOut = () => {
+    signOut(auth).then(() => {});
   };
-  return [user, setUser];
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      setUser(user);
+    });
+  }, []);
+  const signInWithGoogle = () => {
+    signInWithPopup(auth, googlePorvider).then((result) => {
+      const user = result.user;
+      setUser(user);
+      console.log(user);
+    });
+  };
+
+  return { user, signInWithGoogle, handleSignOut };
 };
 
 export default useFirebase;
